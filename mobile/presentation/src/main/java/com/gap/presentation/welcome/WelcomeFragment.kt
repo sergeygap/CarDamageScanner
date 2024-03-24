2a2792b08186f2971afed4281c56ca029717d470
Sergey Gap <1ggserongg1@gmail.com>
2024-03-24 14:57:44 +0300

    add alert dialog

--- mobile/presentation/src/main/java/com/gap/presentation/welcome/WelcomeFragment.kt
+++ mobile/presentation/src/main/java/com/gap/presentation/welcome/WelcomeFragment.kt
@@ -1,10 +1,16 @@
 package com.gap.presentation.welcome
 
+import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import androidx.fragment.app.Fragment
+import com.gap.presentation.R
+import com.gap.presentation.databinding.CustomDialogBinding
 import com.gap.presentation.databinding.FragmentWelcomeBinding
 
 
@@ -13,6 +19,7 @@ class WelcomeFragment : Fragment() {
     private var _binding: FragmentWelcomeBinding? = null
     private val binding: FragmentWelcomeBinding
         get() = _binding ?: throw RuntimeException("WelcomeFragment == null")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,8 +30,26 @@ class WelcomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        workWithUI()
     }
+
+    private fun workWithUI() {
+        with(binding) {
+            btnWelcome.setOnClickListener {
+                openGalleryOrTakePhoto()
+            }
+        }
+    }
+
+    private fun openGalleryOrTakePhoto() {
+        val dialogBinding = CustomDialogBinding.inflate(layoutInflater)
+        val alertDialog = AlertDialog.Builder(
+            requireContext(),
+            R.style.RoundedCornersDialog
+        ).setView(dialogBinding.root).create()
+        alertDialog.show()
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
