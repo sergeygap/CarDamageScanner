3da99e7386d2c2e3263fa1a439c03272b05bc239
Sergey Gap <1ggserongg1@gmail.com>
2024-05-09 23:47:27 +0300

    set up start fragment

--- mobile/presentation/src/main/java/com/gap/presentation/autorization/StartFragment.kt
+++ mobile/presentation/src/main/java/com/gap/presentation/autorization/StartFragment.kt
@@ -5,15 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.gap.presentation.R
+import com.gap.presentation.databinding.FragmentStartBinding
 
 
 class StartFragment : Fragment() {
+
+    private val navController by lazy { findNavController() }
+    private var _binding: FragmentStartBinding? = null
+    private val binding
+        get() = _binding ?: throw RuntimeException("StartFragment == null")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_start, container, false)
+    ): View {
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSign.setOnClickListener {
+            navController.navigate(R.id.action_startFragment_to_loginFragment)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
