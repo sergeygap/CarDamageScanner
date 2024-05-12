def273c636fd274a54fea688cfdca5e85c60df0d
Sergey Gap <1ggserongg1@gmail.com>
2024-05-12 15:51:04 +0300

    fix Report Fragment UI

--- mobile/presentation/src/main/java/com/gap/presentation/main/ReportFragment.kt
+++ mobile/presentation/src/main/java/com/gap/presentation/main/ReportFragment.kt
@@ -5,18 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.gap.presentation.R
+import com.gap.presentation.databinding.FragmentHomeBinding
+import com.gap.presentation.databinding.FragmentReportBinding
+import com.gap.presentation.main.viewModels.HomeViewModel
 
 
 class ReportFragment : Fragment() {
+    private var _binding: FragmentReportBinding? = null
+    private val binding
+        get() = _binding ?: throw RuntimeException("ReportFragment == null")
+    private val navController by lazy { findNavController() }
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false)
+    ): View {
+        _binding = FragmentReportBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        workWithButtons()
+    }
+
+    private fun workWithButtons() {
+        with(binding) {
+            ibBack.setOnClickListener {
+                navController.navigate(R.id.action_reportFragment_to_homeFragment)
+            }
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
--- mobile/presentation/src/main/java/com/gap/presentation/main/adapters/HomeAdapter.kt
+++ mobile/presentation/src/main/java/com/gap/presentation/main/adapters/HomeAdapter.kt
@@ -2,9 +2,12 @@ package com.gap.presentation.main.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.RecyclerView
 import com.gap.data.ScannerRepositoryImpl
 import com.gap.domain.useCases.GetListReportsUseCase
+import com.gap.presentation.R
 import com.gap.presentation.databinding.ItemReportBinding
 
 class HomeAdapter : RecyclerView.Adapter<HomeViewHolder>() {
@@ -24,6 +27,18 @@ class HomeAdapter : RecyclerView.Adapter<HomeViewHolder>() {
         val reportsList = getListReports.invoke()
         val report = reportsList[position]
         viewHolder.binding.tvDate.text = report.title
+        onItemClickListener(viewHolder)
+    }
+
+    private fun onItemClickListener(viewHolder: HomeViewHolder) {
+        viewHolder.itemView.setOnClickListener {
+            (viewHolder.itemView.context as? AppCompatActivity)?.let { activity ->
+                val navHostFragment =
+                    activity.supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+                val navController = navHostFragment.navController
+                navController.navigate(R.id.action_homeFragment_to_reportFragment)
+            }
+        }
     }
 
     override fun getItemCount(): Int = getListReports.invoke().size
